10a51dcdc1f8c0a49c13158b18f2bb286a0e3d5e
LOUISENZR <louisanaza@gmail.com>
2024-02-21 15:39:55 +0700

    input miss

--- Perulangan16.java
+++ Perulangan16.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
 public class Perulangan16 {
     public static void main(String[] args) {
-       int n=17;
-       int i;
+    Scanner sc16 = new Scanner(System.in);
+    System.out.println("Masukan Nim :");
+    String nim = sc16.nextLine();
+    int n = Integer.parseInt(nim.substring(nim.length() -2));
+    if (n<10){
+        n+=10;
+    }
+    int i;
         for (i=1; i<=n; i++){
             if(i !=6 && i!=10){
                 if (i %2 ==0){
