f8110450de49a807204d251bc4759a8907bd7d8c
LOUISENZR <louisanaza@gmail.com>
2024-02-21 20:33:24 +0700

    tugas utama

--- fungsi16.java
+++ fungsi16.java
@@ -1,22 +1,14 @@
 public class fungsi16 {
-    // Harga bunga per potong
-    private static final int[] HARGA_BUNGA = {75000, 50000, 60000, 10000}; // Aglonema, Keladi, Alocasia, Mawar
-
-    // Jumlah stok bunga di setiap cabang
-    private static int[][] STOK_BUNGA = {
-            {10, 5, 15, 7}, // Cabang 1
-            {6, 11, 9, 12}, // Cabang 2
-            {2, 10, 10, 5}, // Cabang 3
-            {5, 7, 12, 9}   // Cabang 4
+    private static final int[] HARGA_BUNGA = {75000, 50000, 60000, 10000};
+    private static final int[][] STOK_BUNGA = {
+            {10, 5, 15, 7},
+            {6, 11, 9, 12},
+            {2, 10, 10, 5},
+            {5, 7, 12, 9}
     };
-
-    // Nama bunga
     private static final String[] NAMA_BUNGA = {"Aglonema", "Keladi", "Alocasia", "Mawar"};
-
-    // Nama cabang
     private static final String[] NAMA_CABANG = {"Royal Garden 1", "Royal Garden 2", "Royal Garden 3", "Royal Garden 4"};
 
-    // Fungsi untuk menampilkan pendapatan setiap cabang jika semua bunga habis terjual
     public static void tampilkanPendapatan() {
         for (int i = 0; i < NAMA_CABANG.length; i++) {
             int pendapatanCabang = 0;
@@ -27,33 +19,31 @@ public class fungsi16 {
         }
     }
 
-    // Fungsi untuk mengurangi stok bunga karena bunga tersebut mati
-    public static void kurangiStokBungaCabang4(String jenisBunga, int jumlah) {
-        int indexBunga = -1;
-        for (int j = 0; j < NAMA_BUNGA.length; j++) {
-            if (NAMA_BUNGA[j].equals(jenisBunga)) {
-                indexBunga = j;
-                break;
+    public static void tampilkanStokBunga() {
+        System.out.println("Jumlah Stok Bunga di Cabang Royal Garden4:");
+        for (int i = 3; i < NAMA_CABANG.length; i++) {
+            System.out.println(NAMA_CABANG[i] + ":");
+            for (int j = 0; j < NAMA_BUNGA.length; j++) {
+                int stokBunga = STOK_BUNGA[i][j];
+                // Pengurangan stok karena bunga mati hanya di cabang keempat
+                if (i == 3) {
+                    if (NAMA_BUNGA[j].equals("Aglonema")) {
+                        stokBunga -= 1;
+                    } else if (NAMA_BUNGA[j].equals("Keladi")) {
+                        stokBunga -= 2;
+                    } else if (NAMA_BUNGA[j].equals("Mawar")) {
+                        stokBunga -= 5;
+                    }
+                }
+                System.out.println("- " + NAMA_BUNGA[j] + ": " + stokBunga);
             }
-        }
-        if (indexBunga != -1) {
-            STOK_BUNGA[3][indexBunga] -= jumlah;
-            System.out.println("Stok bunga " + jenisBunga + " di " + NAMA_CABANG[3] + " dikurangi sebanyak " + jumlah);
-        } else {
-            System.out.println("Jenis bunga tidak ditemukan");
+            System.out.println();
         }
     }
 
     public static void main(String[] args) {
-        // Tampilkan pendapatan setiap cabang jika semua bunga habis terjual
         tampilkanPendapatan();
-
-        // Contoh pengurangan stok bunga karena bunga mati hanya pada Cabang 4
-        kurangiStokBungaCabang4("Aglonema", 1);
-        kurangiStokBungaCabang4("Keladi", 2);
-
-        // Tampilkan stok setiap jenis bunga pada setiap cabang setelah pengurangan
-        System.out.println("\nStok bunga setelah pengurangan:");
+        System.out.println();
         tampilkanStokBunga();
     }
 }
